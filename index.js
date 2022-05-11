4bcf60932391043b10cc5592c5bb32937e081ef3
tevzi2 <tevz.beskovnik@gmail.com>
2022-05-11 22:34:08 +0200

    evo majstor

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+console.log("Reading in html...");
 const html = fs.readFileSync("./main.html", "utf-8").split(/\r?\n/).join("").replace(/\\/g, "\\\\").replace(/"/g, "\\\"");
 //console.log(html)
 const cpp = fs.readFileSync("./src/main.cpp", "utf-8").replace(/String htmlFile = ".*?";\/\/endOfVar/g, "String htmlFile = \""+html+"\";//endOfVar");
---
+++ read_page.py
@@ -0,0 +1,6 @@
+import os
+from platform import platform
+
+Import("env")
+
+os.system("node ./index.js")
